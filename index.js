738f4f0063b109c1d41fb16cecd969d3387d8ee1
Alexander Shtuchkin <ashtuchkin@gmail.com>
2013-08-06 09:45:36 +0400

    Serve static files.

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ app.get('/', function(req, res) {
   res.send(fs.readFileSync("index.html", "utf8"));
 });
 
+app.use(express.static(__dirname));
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
